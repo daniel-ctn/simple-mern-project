03f2e10e0da7dfabd3657c2f3ddd2d93582ede63
Daniel Nguyen <danielnguyen5201@gmail.com>
2021-01-22 00:25:21 +0700

    adjust header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,13 +1,15 @@
-import React from 'react';
-
-import './Header.css';
+import './Header.css'
 
 const Header = props => {
   return (
     <header className="header">
       <h1>MERN Shop</h1>
+      <div className="menu">
+        <h3>Users</h3>
+        <h3>Sign in</h3>
+      </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
